aab484bbfe046eda7d3b2f2e03f4a5c3548a218c
Zhan Zhengwei <zzhan2@sheffield.ac.uk>
2022-03-29 19:36:10 +0800

    a demo for testing paypal.

--- server-app/Payment/models.py
+++ server-app/Payment/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import paypalrestsdk
+import logging
 
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+  "client_id": "AUN871xpue4U-WMyHi3lqgwo8xByk4KA3UykduTfuuVxgZ4ErmqeO17axqEBBQo8ER26PlAAX9As9eYT",
+  "client_secret": "EIdf-Ya9BzSyyegIqfrTHDAfaAKr_QgnqWFLTeHWF7ctY1-_PqYaytNEsP-DhHr9DYa3EfoqNtYmxm2Q" })
+
+payment = paypalrestsdk.Payment({
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"},
+    "redirect_urls": {
+        "return_url": "https://www.baidu.com",
+        "cancel_url": "https://www.google.com"},
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "item",
+                "sku": "item",
+                "price": "5.00",
+                "currency": "GBP",
+                "quantity": 1}]},
+        "amount": {
+            "total": "5.00",
+            "currency": "GBP"},
+        "description": "This is the payment transaction description."}]})
+
+if payment.create():
+  print("Payment created successfully")
+else:
+  print(payment.error)
 # Create your models here.
--- server-app/Payment/tests.py
+++ server-app/Payment/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from paypalrestsdk import BillingPlan
 
+billing_plan = BillingPlan({
+    "name": "Fast Speed Plan",
+    "description": "Create Plan for Regular",
+    "merchant_preferences": {
+        "auto_bill_amount": "yes",
+        "cancel_url": "http://www.paypal.com/cancel",
+        "initial_fail_amount_action": "continue",
+        "max_fail_attempts": "1",
+        "return_url": "http://www.paypal.com/execute",
+        "setup_fee": {
+            "currency": "GBP",
+            "value": "25"
+        }
+    },
+    "payment_definitions": [
+        {
+            "amount": {
+                "currency": "GBP",
+                "value": "100"
+            },
+            "charge_models": [
+                {
+                    "amount": {
+                        "currency": "GBP",
+                        "value": "10.60"
+                    },
+                    "type": "SHIPPING"
+                },
+                {
+                    "amount": {
+                        "currency": "GBP",
+                        "value": "20"
+                    },
+                    "type": "TAX"
+                }
+            ],
+            "cycles": "0",
+            "frequency": "MONTH",
+            "frequency_interval": "1",
+            "name": "Regular 1",
+            "type": "REGULAR"
+        }
+    ],
+    "type": "INFINITE"
+})
+
+response = billing_plan.create()
+print(response)
 # Create your tests here.
